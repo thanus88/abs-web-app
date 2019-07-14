e5dae19290232bbd222fca6e611688a41729386f
thanus88 <m.sanla2525@gmail.com>
2019-07-14 17:46:38 +0700

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 6';
+  safeSrc: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) { 
+    this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/c9F5kMUfFKk");
+  }
+
+}
