d89e476cdc91eb8256b1359f11567116822ba3eb
thanus88 <m.sanla2525@gmail.com>
2019-08-04 17:42:15 +0700

    implement with google facebook authen.

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -9,7 +9,8 @@ import { Events } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { AuthenService } from '../../providers/AuthenService';
 
-
+declare const FB: any;
+declare const googleUser : any;
 
 @Component({
   selector: 'page-login',
@@ -31,7 +32,24 @@ export class LoginPage {
     public AuthenService:AuthenService,
     public events: Events,
     public storage: Storage
-  ) { }
+  ) {
+    /*
+    FB.init({
+      appId: '501447963923481',
+      cookie: true,
+      xfbml: true,  //parse social plugins on this page
+      version: 'v4.0' //use graph api version 2.5
+    });
+    (function (d, s, id, appId) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0&appId=" 
+    + appId; //<--passing config value here
+      fjs.parentNode.insertBefore(js, fjs);
+    } (document, 'script', 'facebook-jssdk', '501447963923481'));
+    */
+   }
 
   onLogin(form: NgForm) {
     this.submitted = true;
@@ -53,5 +71,43 @@ export class LoginPage {
 
   onSignup() {  //  Re Direct TO Signup page.
     this.router.navigateByUrl('/signup');
+  }
+
+  onSignInByGoogle(){
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+  }
+
+  onLoginByFacebook(form: NgForm) {
+    this.submitted = false;
+    console.log('onLoginByFacebook..');
+    //this.checkLoginState();
+    this.router.navigateByUrl('/signup');
+  }
+
+  statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      this.router.navigateByUrl('/app/tabs/(rss-feed:rss-feed)');
+    } else {
+      // The person is not logged into your app or we are unable to tell.
+      this.router.navigateByUrl('/signup');
+    }
+  }
+
+  checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      console.log(response);
+      this.statusChangeCallback(response);
+    });
   }
 }
